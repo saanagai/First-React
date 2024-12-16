c3094625a14f600d02fdf40e0c1f81d051349957
24LP0542 24LP0542 <24LP0542@G304-CC0227.local>
2024-12-16 11:58:32 +0800

    React-first

---
+++ server.js
@@ -0,0 +1,6 @@
+import App from from "./App";
+import ReactDom from "react-dom/client";
+
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+/// deerh bichilt zaaval bichigene.
+
+import React from "react";
+import { Scripts } from "react-router-dom";
+const App = () => {
+  return <div>Done</div>;
+};
+export default App;
+
+///jsx file ni js html 2g zereg hiij bno.
+// react -Scripts avtomataar uusgene.
+//
